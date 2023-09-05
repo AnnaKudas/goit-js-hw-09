65875472a53d513dc7e82289ed947efdd3ff480a
AnnaKudas <126461979+AnnaKudas@users.noreply.github.com>
2023-09-05 14:56:57 +0000

    Deploying to gh-pages from @ AnnaKudas/goit-js-hw-09@7f36854dce91fd00545bcad820c8111ee8d8da57 🚀

---
+++ 01-color-switcher.b3173b64.js
@@ -0,0 +1,2 @@
+!function(){var t=null;document.addEventListener("DOMContentLoaded",(function(){var e=document.querySelector("[data-start]"),n=document.querySelector("[data-stop]");e.addEventListener("click",(function(){t||(t=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,"0"))}),1e3),e.disabled=!0,n.disabled=!1)})),n.addEventListener("click",(function(){t&&(clearInterval(t),t=null,e.disabled=!1,n.disabled=!0)}))}))}();
+//# sourceMappingURL=01-color-switcher.b3173b64.js.map
