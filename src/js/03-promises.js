107d0d31d287d29f376d9556d9e21f764df2d884
Anna Kudas <nnkudas@gmail.com>
2023-09-07 11:50:44 +0200

    task 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,56 +1,39 @@
-// TASK 3
-// Завдання 3 - генератор промісів
-// Виконуй це завдання у файлах 03-promises.html і 03-promises.js. Подивися демо-відео роботи генератора промісів.
+const button = document.querySelector('button');
 
-// HTML містить розмітку форми, в поля якої користувач буде вводити першу затримку в мілісекундах, крок збільшення затримки для кожного промісу після першого і кількість промісів, яку необхідно створити.
-
-// <form class="form">
-//   <label>
-//     First delay (ms)
-//     <input type="number" name="delay" required />
-//   </label>
-//   <label>
-//     Delay step (ms)
-//     <input type="number" name="step" required />
-//   </label>
-//   <label>
-//     Amount
-//     <input type="number" name="amount" required />
-//   </label>
-//   <button type="submit">Create promises</button>
-// </form>
-
-// Напиши скрипт, який на момент сабміту форми викликає функцію createPromise(position, delay) стільки разів, скільки ввели в поле amount. Під час кожного виклику передай їй номер промісу (position), що створюється, і затримку, враховуючи першу затримку (delay), введену користувачем, і крок (step).
-
-// function createPromise(position, delay) {
-//   const shouldResolve = Math.random() > 0.3;
-//   if (shouldResolve) {
-//     // Fulfill
-//   } else {
-//     // Reject
-//   }
-// }
-
-// Доповни код функції createPromise таким чином, щоб вона повертала один проміс, який виконується або відхиляється через delay часу. Значенням промісу повинен бути об'єкт, в якому будуть властивості position і delay зі значеннями однойменних параметрів. Використовуй початковий код функції для вибору того, що потрібно зробити з промісом - виконати або відхилити.
-
-// createPromise(2, 1500)
-//   .then(({ position, delay }) => {
-//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-//   })
-//   .catch(({ position, delay }) => {
-//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-//   });
-
-// Бібліотека повідомлень
-// УВАГА
-// Наступний функціонал не обов'язковий для здавання завдання, але буде хорошою додатковою практикою.
-
-// Для відображення повідомлень користувачеві, замість console.log(), використовуй бібліотеку notiflix.
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  promise = new Promise((resolve, reject) => {
+    if (shouldResolve) {
+      resolve({ position, delay });
+    } else {
+      reject({ position, delay });
+    }
+  });
+
+  return promise;
 }
+
+button.addEventListener("click", (evt) => {
+  evt.preventDefault(); 
+  const amount = parseInt(document.getElementsByName("amount")[0].value, 10);
+  const initialDelay = parseInt(document.getElementsByName("delay")[0].value, 10);
+  const step = parseInt(document.getElementsByName("step")[0].value, 10);
+
+  if (isNaN(amount) || isNaN(initialDelay) || isNaN(step)) {
+      alert("Please enter valid numeric values.");
+      return;
+  }
+
+  for (let i = 0; i < amount; i++) {
+      const position = i + 1;
+      const delay = initialDelay + i * step;
+      
+      createPromise(position, delay)
+        .then(({ position, delay }) => {
+        console.log(` ✅ Fulfilled promise ${position} in ${delay}ms`);
+        })
+        .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+        });
+  }
+}) 
